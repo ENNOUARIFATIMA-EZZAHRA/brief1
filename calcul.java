b4a784b0aa9bb4f23ea09019b8e214ce4a7b7193
ENNOUARI FATIMA-EZZAHRA <fatimaezzahraennouari@gmail.com>
2024-12-11 10:26:53 +0100

    create menu

---
+++ calcul.java
@@ -0,0 +1,18 @@
+public class Main {
+	    public static void main(String[] args) {
+            int a, b;
+
+            System.out.println("*******Menu Principale*******\n");
+            System.out.println("1-Addition(+)\n");
+            System.out.println("2-Soustraction(-)\n");
+            System.out.println("3-Multiplication(*)\n");
+            System.out.println("4-Division(/)\n");
+            System.out.println("5-Puissance\n");
+            System.out.println("6-Racine carré\n");
+            System.out.println("7-Factorielle\n");
+            System.out.println("8-Quitter\n");
+            System.out.println("Choisé un choix :\n");
+
+
+    }
+}
